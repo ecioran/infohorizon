db0b54d079c80bdec244df4f9c80c8c32dabf32a
ecioran <88317238+ecioran@users.noreply.github.com>
2022-06-14 19:12:11 +0500

    Delete docusaurus.config.js

--- docusaurus.config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  url: 'https://spacepenguin.netlify.app', // Url to your site with no trailing slash
-  baseUrl: '/infohorizon', // Base directory of your site relative to your repo
-  // ...
-};
