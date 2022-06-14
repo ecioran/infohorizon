d0bfc2fa07857889f27ce17a71ff08940d085928
ecioran <88317238+ecioran@users.noreply.github.com>
2022-06-14 18:40:53 +0500

    Create docusaurus.config.js

---
+++ docusaurus.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  // ...
+  url: 'https://ecioran.github.io', // Your website URL
+  baseUrl: '/',
+  projectName: 'ecioran.github.io',
+  organizationName: 'ecioran',
+  trailingSlash: false,
+  // ...
+};
